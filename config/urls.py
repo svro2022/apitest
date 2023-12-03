13c701ab1222452155ddc4058a97fec0a0ea7347
svro <ichistrawberry2012@gmail.com>
2023-12-03 18:12:35 +0300

    products_viewset

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
+    path('', include('products.urls', namespace='products')),
 ]
